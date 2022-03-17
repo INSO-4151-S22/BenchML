e1c0c0e1861dfa7a77388e0d63b6054f2e748fb2
FernandoDavis <fernando.davis@upr.edu>
2022-03-17 03:47:55 -0400

    Added pytorch model creation with configurable hyperparameters for use with ray tune.

--- benchmarking/optimize.py
+++ benchmarking/optimize.py
@@ -13,22 +13,22 @@ class Optimize:
     def __init__(self):
         pass
 
-    def create_model_from_args(self, model_type, layers, config):
+    def create_model_from_args(self, config):
         # Model type for now only supports keras and pytorch
-        if model_type == 'keras':
+        if config['model_type'] == 'keras':
             return self.create_model_keras()
-        elif model_type == 'pytorch':
-            return self.create_model_pytorch()
+        elif config['model_type'] == 'pytorch':
+            return self.create_model_pytorch(config)
         else: 
             return None
 
-    def create_model_keras(self, model_type, layers, config):
+    def create_model_keras(self, config):
         '''
         Creates a keras model based on given structure with the usage of config values in hyperparameters.
         '''
         return None
     
-    def create_model_pytorch(self, layers, config):
+    def create_model_pytorch(self, config):
         '''
         Creates a pytorch model based on given structure with the usage of config values in hyperparameters.
         '''
@@ -39,29 +39,29 @@ class Optimize:
                 # Define layers based on init Args
                 self.model = []
                 for layer in layers:
-                    if layer[0] == "Conv2D":
+                    if layer[0] == "Conv2d":
                         # Extract parameters for Conv2D layer 
                         layer_args = []
                         for i in range(1, len(layer)):
-                            if config[layer[i]]:
+                            if isinstance(layer[i], str) and config[layer[i]]:
                                 layer_args.append(config[layer[i]])  # Applies a config value to it if available
                             else:
                                 layer_args.append(layer[i])
-                        self.model.append(nn.Conv2D(*layer_args))  # Creates a Conv2D layer with the appropiate parameters
-                    elif layer[0] == "MaxPool2D":
+                        self.model.append(nn.Conv2d(*layer_args))  # Creates a Conv2D layer with the appropiate parameters
+                    elif layer[0] == "MaxPool2d":
                         # Extract parameters for MaxPool2D layer 
                         layer_args = []
                         for i in range(1, len(layer)):
-                            if config[layer[i]]:
+                            if isinstance(layer[i], str) and config[layer[i]]:
                                 layer_args.append(config[layer[i]])  # Applies a config value to it if available
                             else:
                                 layer_args.append(layer[i])
-                        self.model.append(nn.MaxPool2D(*layer_args))  # Creates a MaxPool2D layer with the appropiate parameters
+                        self.model.append(nn.MaxPool2d(*layer_args))  # Creates a MaxPool2D layer with the appropiate parameters
                     elif layer[0] == "Linear":
                         # Extract parameters for Linear layer 
                         layer_args = []
                         for i in range(1, len(layer)):
-                            if config[layer[i]]:
+                            if isinstance(layer[i], str) and config[layer[i]]:
                                 layer_args.append(config[layer[i]])  # Applies a config value to it if available
                             else:
                                 layer_args.append(layer[i])
@@ -71,6 +71,8 @@ class Optimize:
                         for i in range(1, len(layer)):
                             view_args.append(layer[i])
                         self.model.append(view_args)
+                    elif layer[0] == "relu":
+                        self.model.append(nn.ReLU)
                     else:
                         raise ArgumentTypeError("The layer {} is incompatible with the optimization model creation for this current version.".format(layer[0]))
 
@@ -83,13 +85,32 @@ class Optimize:
                         x = fun(x)
                 return x
 
-        return Net(layers, config)
+            def to_string(self):
+                string = ""
+                i = 0
+                for fun in self.model:
+                    if isinstance(fun, list) and fun[0] == "view":
+                        string += "Layer {}: x.view(".format(i)
+                        for index in range(1, len(fun)):
+                            string += " {} ".format(fun[index])
+                        string += ")\n"
+                    elif isinstance(fun, type):
+                        string += "Layer {}: {}\n".format(i, fun.__name__)
+                    else:
+                        string += "Layer {}: {}\n".format(i, fun._get_name)
+                    i+=1
+                return string
+
+        return Net(config["layers"], config)
 
     def train_cifar(self, config):
-        pass
+        net = Optimize.create_model_from_args(self, config)
+        return net
 
     def data_loader(self):
         pass
 
+
+    
     
     
