a5c9e0907168dd4b862288112f57ca33ce165fc4
eviera21 <enrique.viera@upr.edu>
2022-04-30 18:00:22 -0400

    added login
     not working yet (switching to laptop)

--- frontend/src/Components/MyModelsTable.js
+++ frontend/src/Components/MyModelsTable.js
@@ -1,67 +1,70 @@
-import React, { useMemo, useState, useEffect } from 'react'
+import React, { useMemo } from 'react'
 import { useTable } from 'react-table'
 // import MOCK_DATA from './MOCK_DATA.json'
 import { COLUMNS } from './Columns'
 import '../css/MyModelsTable.min.css'
 import axios from 'axios'
 
+const baseURL = "https://benchml.gabrielrosa.dev/models";
 export function BasicTable() {
-    const [models, setModels] = useState(null);
-    useEffect(() => {
-        axios.get("http://localhost:8000/mymodels").then((response) => {
+    const [Models, setModels] = React.useState(null);
+
+    React.useEffect(() => {
+        axios.get(baseURL).then((response) => {
             setModels(response.data);
+            console.log(response);
         });
     }, []);
     const columns = useMemo(() => COLUMNS, [])
-    const data = useMemo(() => models.data, [])
 
-    const tableInstance = useTable({
-        columns,
-        data
-    })
+    // const tableInstance = useTable({
+    //     columns,
+    //     Models
+    // })
 
-    const { 
-        getTableProps, 
-        getTableBodyProps,
-         headerGroups, 
-         rows, 
-         prepareRow,
-    } = tableInstance
+    // const { 
+    //     getTableProps, 
+    //     getTableBodyProps,
+    //      headerGroups, 
+    //      rows, 
+    //      prepareRow,
+    // } = tableInstance
 
     return (
+        <h1>{Models}</h1>
 
-        <table {...getTableProps()}>
-            <thead>
-                {
-                    headerGroups.map((headerGroup) => (
-                    <tr {...headerGroup.getHeaderGroupProps()} >
-                    {
-                        headerGroup.headers.map(column => (
-                            <th {...columns.getHeaderProps}>{column.render('Header')}</th>
-                        ))
-                    }
-                    </tr>
+        // <table {...getTableProps()}>
+        //     <thead>
+        //         {
+        //             headerGroups.map((headerGroup) => (
+        //             <tr {...headerGroup.getHeaderGroupProps()} >
+        //             {
+        //                 headerGroup.headers.map(column => (
+        //                     <th {...columns.getHeaderProps}>{column.render('Header')}</th>
+        //                 ))
+        //             }
+        //             </tr>
 
-                ))}
+        //         ))}
                 
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {
-                    rows.map(row => {
-                        prepareRow(row)
-                        return (
-                            <tr {...row.getRowProps()}> 
-                            {
-                                row.cells.map((cell) => {
-                                    return <td{...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                })}
+        //     </thead>
+        //     <tbody {...getTableBodyProps()}>
+        //         {
+        //             rows.map(row => {
+        //                 prepareRow(row)
+        //                 return (
+        //                     <tr {...row.getRowProps()}> 
+        //                     {
+        //                         row.cells.map((cell) => {
+        //                             return <td{...cell.getCellProps()}>{cell.render('Cell')}</td>
+        //                         })}
                             
-                            </tr>
-                        )
-                    })
-                }
+        //                     </tr>
+        //                 )
+        //             })
+        //         }
                
-            </tbody>
-        </table>
+        //     </tbody>
+        // </table>
     )
 }
